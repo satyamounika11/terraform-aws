e9d04cdef13032e38e29f7fea910d3c220fdf5ac
Sasi Datta <dattu2009@yahoo.com>
2024-07-04 14:39:07 +0530

    Create ec2.tf
    Creating a sample EC2 resource

---
+++ ec2.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "example" {
+  ami           = "ami-0abcdef1234567890"  # specify a valid AMI ID
+  instance_type = "t2.micro"  # specify the instance type
+
+  tags = {
+    Name = "ExampleInstance"
+  }
+}
