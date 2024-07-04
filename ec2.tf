52fa281c1b5b87dbd46a210764cf8dec75a71ee5
Sasi Datta <sasi.bitra@mitel.com>
2024-07-04 14:47:45 +0530

    Corrected AMI

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "example" {
-  ami           = "ami-0abcdef1234567890" # specify a valid AMI ID
+  ami           = "ami-04a81a99f5ec58529" # specify a valid AMI ID
   instance_type = "t2.micro"              # specify the instance type
 
   tags = {
