ea785792824c6d5c7957f0fb0168a01f660ab193
Sasi Datta <sasi.bitra@mitel.com>
2024-07-04 14:43:32 +0530

    Adding EC2 fmt check

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "example" {
-  ami           = "ami-0abcdef1234567890"  # specify a valid AMI ID
-  instance_type = "t2.micro"  # specify the instance type
+  ami           = "ami-0abcdef1234567890" # specify a valid AMI ID
+  instance_type = "t2.micro"              # specify the instance type
 
   tags = {
     Name = "ExampleInstance"
