dcfcc353767de901993e18f58ed952d1cbaf3ec8
Satya Mounika <37068004+satyamounika11@users.noreply.github.com>
2024-07-04 14:20:19 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "demo-github-action-tf"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock" 
+    encrypt        = true
+  }
+}
